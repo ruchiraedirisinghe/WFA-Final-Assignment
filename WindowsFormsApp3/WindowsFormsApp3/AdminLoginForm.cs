5d4c9d7036f33e1e7a48efce20d32d895a32e944
Sudeera Wijenayake <76869643+sudeera1153@users.noreply.github.com>
2021-08-15 01:11:22 +0530

    Admin Update to HAsan

--- WindowsFormsApp3/WindowsFormsApp3/AdminLoginForm.cs
+++ WindowsFormsApp3/WindowsFormsApp3/AdminLoginForm.cs
@@ -21,7 +21,7 @@ namespace WindowsFormsApp3
 
         private void gunaAdvenceButton1_Click(object sender, EventArgs e)
         {
-            if ((txt_AdminName.Text == "admin" && txt_AdminPw.Text == "admin")||(txt_AdminName.Text == "nimna" && txt_AdminPw.Text == "ruchira"))
+            if ((txt_AdminName.Text == "admin" && txt_AdminPw.Text == "admin")||(txt_AdminName.Text == "hasan" && txt_AdminPw.Text == "aiaa"))
                 {
                     AdminMain adminmainform = new AdminMain();
                     this.Hide();
